be7c9bba2556b2600b9af877b364176df98b3c89
Rich Howard <richardahoward@gmail.com>
2014-07-14 11:41:37 -0700

    massive functionality upgrade

--- wrapper.sh
+++ wrapper.sh
@@ -2,59 +2,65 @@
 # written by zeroecco for all those times
 # I needed a cron task wrapper and had to re-write one
 
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/P
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -eu
 set -o pipefail
 IFS=$'\n\t'
 
-# TODO - make these configurable
-local LOCK_FILE="/tmp/shell_wrapper.lock"
-local LOG_DIR='/var/log/'
-local LOG_FILE='/var/log/shell_wrapper.log'
-local STATUS='LOCKED'
-
-if [[ ! -d $LOG_DIR ]]; then
-  mkdir $LOG_DIR
-fi
-
-# make sure we can write to the log dir
-if [[ ! -w $LOG_DIR ]]; then
-  echo "Cannot write to $LOG_DIR as this user: $USER, exiting"
-  exit 1
-fi
-
 # A basic logging function for reviews
 log() {
-  # Log Date Format
-  DATE=$(date +"%Y-%m-%d %H:%M:%S %z")
-  echo -e "$DATE\t$1" >> "$LOG_FILE"
+  local MESSAGE=$1
+  local DATE=$(date +"%Y-%m-%d %H:%M:%S %z")
+
+  echo -e "$DATE\t$MESSAGE" >> "$LOG_FILE"
 }
+
+log_mkdir() {
+  local LOG_DIR=$1
+
+  if [[ ! -d $LOG_DIR ]]; then
+    mkdir "$LOG_DIR"
+  fi
+}
+
+log_writeable() {
+  if [[ ! -w $LOG_DIR ]]; then
+    echo "Cannot write to $LOG_DIR as this user: $USER, exiting"
+    exit 1
+  fi
+}
+
+locked() {
+  if [[ -e $LOCK_FILE ]]; then
+    log "Lock file exists, exiting." >> "$LOG_FILE"
+    exit 1
+  elif [[ ! -e $LOCK_FILE ]]; then
+    log "No lock file exists, moving forward" >> "$LOG_FILE"
+  fi
+}
+
+run_command() {
+  local CMD=$1
+  log "$CMD is beginning its' run"
+  # TODO - make this more intellegent
+  eval "$CMD" >> "$LOG_FILE"
+}
+
 
 # The lock function
 lock() {
   # Lock the cron job
   log "Creating lock file for: $COMMAND"
   # TODO - catch on error
-  touch $LOCK_FILE
+  touch "$LOCK_FILE"
 }
 
 # The unlock function
 unlock() {
   # unlock the cron job
   log "Removing lock file"
-  rm $LOCK_FILE
+  rm "$LOCK_FILE"
 }
-
-status() {
-  if [[ -e $LOCK_FILE ]]; then
-    STATUS='LOCKED'
-  fi
-
-  if [[ ! -e $LOCK_FILE ]]; then
-    STATUS='UNLOCKED'
-  fi
-}
-
 
 while getopts ":c:" opt; do
   case "$opt" in
@@ -63,37 +69,18 @@ while getopts ":c:" opt; do
   esac
 done
 
-log "$COMMAND is beginning its' run"
+main() {
+  local LOCK_FILE="/tmp/shell_wrapper.lock"
+  local LOG_DIR='/var/log/'
+  local LOG_FILE='/var/log/shell_wrapper.log'
 
-# Check the lock file so that a command is not
-# run on top of itself
-if [ -e $LOCK_FILE ]
-then
-  log "$COMMAND lock file exists... exiting"
-  exit 1
-fi
-
-# Create the lock file for the command to be ran
-lock
-
-
-# Run the command
-# TODO - make this more intellegent
-$COMMAND >> $LOG_FILE
-
-# If command fails, do not re-run
-if [[ $? -ne 0 ]]; then
-  log "$COMMAND exited with a non-zero status. exiting without removing the lock file."
-  exit 1
-fi
-
-#delete lock file at end of your job
-unlock
-
-if [[ ! -f $LOCK_FILE ]]; then
-  log "$COMMAND completed successfully."
+  locked
+  log_writeable
+  log_mkdir "$LOG_DIR"
+  lock
+  run_command "$1"
+  unlock
+  log "task completed, exiting"
   exit 0
-else
-  log "$LOCK_FILE is still there, removal of lock file likely did not complete. Please investigate."
-  exit 1
-fi
+}
+main "$COMMAND"
